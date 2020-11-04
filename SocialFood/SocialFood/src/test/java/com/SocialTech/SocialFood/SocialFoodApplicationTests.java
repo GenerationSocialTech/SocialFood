f39649676337ae1fd2f2066d1ce56c36faed21f1
lucas Trevizan <lucasevizan@hotmail.com>
2020-11-04 14:06:30 -0300

    04/11/2020

---
+++ SocialFood/SocialFood/src/test/java/com/SocialTech/SocialFood/SocialFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SocialTech.SocialFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
