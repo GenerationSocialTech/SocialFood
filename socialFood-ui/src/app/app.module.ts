17a297416cdf29bea0ad7f7ed85e66b3543b891a
Marcos Vinicius <jacintomarcos1@hotmail.com>
2020-12-09 08:12:34 -0300

    mudança na home

--- socialFood-ui/src/app/app.module.ts
+++ socialFood-ui/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';                                                                   /*import { HomeComponent } from './home/home.component';*/
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
